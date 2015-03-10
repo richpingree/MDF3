5575b3e505b080277c5565d6906858a0ba71767b
richpingree <richpingree@fullsail.edu>
2015-03-10 09:06:00 -0500

    moved UI and button functionality to fragment
    moved UI and button functionality to fragment

--- MyApplication/app/src/main/java/com/richardpingree/servicefundamentals/MainActivity.java
+++ MyApplication/app/src/main/java/com/richardpingree/servicefundamentals/MainActivity.java
@@ -9,21 +9,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.ResultReceiver;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 
 /**
  * Created by Richard Pingree MDF3 1503 on 3/7/15.
  */
-public class MainActivity extends Activity implements ServiceConnection {
+public class MainActivity extends Activity implements ServiceConnection, mainFragment.OnButtonClickListener{
 
     public static final String TAG = "MainActivity.TAG";
 
-    Button previous, stop, play, pause, next;
-    TextView songTitle;
+    //Button previous, stop, play, pause, next;
+    String songTitleString;
     ServiceClass myService;
     ServiceClass.BoundServiceBinder binder;
     public static final String EXTRA_RECEIVER = "EXTRA_RECEIVER";
@@ -36,12 +33,16 @@ public class MainActivity extends Activity implements ServiceConnection {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        previous = (Button) findViewById(R.id.prevBtn);
-        stop = (Button) findViewById(R.id.stopBtn);
-        play = (Button) findViewById(R.id.playBtn);
-        pause = (Button) findViewById(R.id.pauseBtn);
-        next = (Button) findViewById(R.id.nextBtn);
-        songTitle = (TextView) findViewById(R.id.songTxt);
+        if(savedInstanceState == null){
+            getFragmentManager().beginTransaction().replace(R.id.container, new mainFragment()).commit();
+        }
+
+//        previous = (Button) findViewById(R.id.prevBtn);
+//        stop = (Button) findViewById(R.id.stopBtn);
+//        play = (Button) findViewById(R.id.playBtn);
+//        pause = (Button) findViewById(R.id.pauseBtn);
+//        next = (Button) findViewById(R.id.nextBtn);
+//        songTitle = (TextView) findViewById(R.id.songTxt);
 
         Intent intent = new Intent(this, ServiceClass.class);
         intent.putExtra(EXTRA_RECEIVER, new DataReceiver());
@@ -58,17 +59,23 @@ public class MainActivity extends Activity implements ServiceConnection {
         @Override
         protected void onReceiveResult(int resultCode, Bundle resultData) {
             if(resultData != null && resultData.containsKey(DATA_RETURNED)){
-                songTitle.setText(resultData.getString(DATA_RETURNED, "newSong"));
+                //songTitle.setText(resultData.getString(DATA_RETURNED, "newSong"));
             }
         }
     }
 
     public void songTitle(){
-        songTitle.setText(myService.songNames[myService.mAudioPosition]);
+        //songTitle.setText(myService.songNames[myService.mAudioPosition]);
+    }
+
+    public String getSongTitle(String titleString){
+        songTitleString = myService.songNames[myService.mAudioPosition];
+        titleString = songTitleString;
+        return titleString;
     }
 
     @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
 
         Intent playIntent = new Intent(this, ServiceClass.class);
@@ -76,48 +83,74 @@ public class MainActivity extends Activity implements ServiceConnection {
         startService(playIntent);
 
 
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myService.onStop();
-            }
-        });
+//        stop.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myService.onStop();
+//            }
+//        });
+//
+//        play.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myService.play();
+//                songTitle();
+//            }
+//        });
+//
+//        pause.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(myService.player.isPlaying()){
+//                    myService.onPause();
+//                }else{
+//                    myService.onResume();
+//                }
+//
+//            }
+//        });
+//
+//        previous.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myService.onPrev();
+//                songTitle();
+//            }
+//        });
+//
+//        next.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myService.onNext();
+//                songTitle();
+//            }
+//        });
+    }
+    public void clickStop(){
+        myService.onStop();
+    }
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myService.play();
-                songTitle();
-            }
-        });
+    public void clickPause(){
+        if(myService.player.isPlaying()){
+            myService.onPause();
+        }else{
+            myService.onResume();
+        }
+    }
 
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(myService.player.isPlaying()){
-                    myService.onPause();
-                }else{
-                    myService.onResume();
-                }
+    public void clickPlay(){
+        myService.play();
+        //songTitle();
+    }
 
-            }
-        });
+    public void clickPrev(){
+        myService.onPrev();
+        //songTitle();
+    }
 
-        previous.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myService.onPrev();
-                songTitle();
-            }
-        });
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myService.onNext();
-                songTitle();
-            }
-        });
+    public void clickNext(){
+        myService.onNext();
+        //songTitle();
     }
 
 
